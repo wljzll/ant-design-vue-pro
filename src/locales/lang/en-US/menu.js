33d09f0efb968d4d642d131340639362a63896f7
zoulele <zoulele@xinye.com>
2021-11-01 19:37:24 +0800

    README.md

---
+++ src/locales/lang/en-US/menu.js
@@ -0,0 +1,11 @@
+// 菜单英文翻译
+export default {
+    'menu.welcome': 'Welcome',
+    'menu.home': 'Home',
+    'menu.dashboard': 'Dashboard',
+    'menu.dashboard.analysis': 'Analysis',
+    'menu.dashboard.monitor': 'Monitor',
+    'menu.dashboard.workplace': 'Workplace',
+    'menu.account.trigger': 'Trigger Error',
+    'menu.account.logout': 'Logout'
+}
